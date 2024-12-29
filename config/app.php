85a909bd5988d41268f140ca95c93d09b20c46e6
README.md <177797972+mizuka-k@users.noreply.github.com>
2024-12-29 23:32:45 +0900

    change

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -183,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
     ])->toArray(),
 
 ];
